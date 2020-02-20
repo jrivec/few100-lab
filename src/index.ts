2b071491434b663d121adeddca8408662651caeb
jrivec <61019020+jrivec@users.noreply.github.com>
2020-02-20 16:58:34 +0000

    added local stotrage

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import './styles.css';
 
 const inputBillAmount = document.getElementById('inputBillAmount') as HTMLInputElement;
+const customTipAmount = document.getElementById('customTipAmount') as HTMLInputElement;
+
 const displayBillAmount = document.getElementById('displayBillAmount') as HTMLLIElement;
 const displayTipPercentage = document.getElementById('displayTipPercentage') as HTMLLIElement;
 const displayPercentagecomment = document.getElementById('displayPercentagecomment') as HTMLSpanElement;
@@ -9,24 +11,57 @@ const displayTotal = document.getElementById('displayTotal') as HTMLLIElement;
 const button10 = document.getElementById('ten') as HTMLButtonElement;
 const button15 = document.getElementById('fifteen') as HTMLButtonElement;
 const button20 = document.getElementById('twenty') as HTMLButtonElement;
+const buttonCustom = document.getElementById('custom') as HTMLButtonElement;
 const buttonGroup = document.getElementsByClassName('.btn-group') as unknown as HTMLDivElement;
 
-const allButtons = [button10, button15, button20];
-allButtons.forEach(btn => btn.addEventListener('click', checkButtons));
-inputBillAmount.addEventListener('keyup', setBillAmount);
-
-const savedPercentage = false;
+const allButtons = [button10, button15, button20, buttonCustom];
+let tp = 0;
+let customTp = 0;
 console.log('global');
 
+allButtons.forEach(btn => btn.addEventListener('click', checkButtons));
+inputBillAmount.addEventListener('keyup', setBillAmount);
+customTipAmount.addEventListener('keyup', updateCustomButtonText);
+
+function updateCustomButtonText() {
+    customTp = Number.parseInt(customTipAmount.value);
+    buttonCustom.innerText = `${customTp}%`;
+    if (buttonCustom.disabled === true) {
+        calculateAndUpdate();
+    }
+}
+
+
+const savedPercentage = localStorage.getItem('savedTipPercentage');
 if (savedPercentage) {
-    // set button here
+    switch (savedPercentage) {
+        case '10':
+            tp = 10;
+            button10.disabled = true;
+            setTipPercentage();
+            break;
+        case '15':
+            tp = 15;
+            button15.disabled = true;
+            setTipPercentage();
+            break;
+        case '20':
+            tp = 20;
+            button20.disabled = true;
+            setTipPercentage();
+            break;
+        default:
+            customTp = Number.parseInt(savedPercentage);
+            tp = customTp;
+            buttonCustom.disabled = true;
+            setTipPercentage();
+    }
 } else {
     button10.disabled = false;
     button15.disabled = false;
     button20.disabled = false;
+    buttonCustom.disabled = false;
 }
-
-let tp = 0;
 
 function ShouldRunCalculate(): boolean {
     // if a button is selected and there is anything in bill amount
@@ -57,7 +92,12 @@ function checkButtons() {
             tp = 20;
             setTipPercentage();
             break;
+        case buttonCustom:
+            tp = customTp;
+            setTipPercentage();
+            break;
     }
+    localStorage.setItem('savedTipPercentage', tp.toString());
     calculateAndUpdate();
 }
 
