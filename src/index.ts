b2220ec9e603df939044a26795b21f14c4bf75e8
jrivec <61019020+jrivec@users.noreply.github.com>
2020-02-20 13:40:48 +0000

    bill amount enterable

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
 
+const inputBillAmount = document.getElementById('imputBillAmount') as HTMLInputElement;
+const displayBillAmount = document.getElementById('displayBillAmount') as HTMLLIElement;
+const displayTipPercentage = document.getElementById('displayTipPercentage') as HTMLLIElement;
+const displayAmountOfTip = document.getElementById('displayAmountOfTip') as HTMLLIElement;
+const displayTotal = document.getElementById('displayTotal') as HTMLLIElement;
+
+inputBillAmount.addEventListener('keyup', setBillAmount);
+
+function setBillAmount() {
+    displayBillAmount.innerText = `Bill Amount: $${inputBillAmount.value}`;
+}
